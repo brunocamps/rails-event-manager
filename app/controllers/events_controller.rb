a57a30adbcf9af1a7db48f0de5bf79b94d9c400e
Bruno Campos <bruno.mcamps@outlook.com>
2023-11-25 08:52:52 -0300

    new migration

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  def index
+  def index # This is a view (template)
     # @age = rand(100) # this is an instance variable
     # @events = ["BugSmash", "Hackathon", "Kata Camp", "Rails User Group"]
     @events = Event.all
